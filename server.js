0313df3f72dfd2570fad603ee7e946425cb13268
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-05-12 22:25:54 -0600

    Revert "Fixed cors"
    This reverts commit 91132fddda4de0802e2939a98a21139dbc0c95f6.

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ const port = 8080
 require('mongodb').MongoClient;
 const mongodb = require('./database/db');
 const bodyParser = require('body-parser');
-// app.set('view engine', 'ejs');
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 const cors = require('cors');
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json())
-.use(express.urlencoded({ extended: true }))
 .use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin');
+  res.setHeader('Content-Type');
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 })
-
-app.use("/", require("./routes/"));
+.use("/", require("./routes/"));
 
 
 mongodb.initDb((err, mongodb) => {
