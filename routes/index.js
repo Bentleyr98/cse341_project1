35a66ae461564c90ed43e3ce771d116fd79136d1
Bentleyr98 <bentley.rebekah314@gmail.com>
2023-05-12 22:08:30 -0600

    swagg

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 const baseController = require("../controllers/index.js")
 
-router.get("/", baseController.sendName)
-router.use("/contacts", require("./contacts"))
-
-router.use('/api-docs', require("./swagger"));
-
+router.get("/", baseController.sendName);
+router.use('/', require('./swagger'));
+router.use('/contacts', require('./contacts'));
 
 
 module.exports = router;
